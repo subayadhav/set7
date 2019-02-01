d5a96ad8016e24d6e6636c1b7766657031173b72
subayadhav <suba10499@gmail.com>
2019-02-01 23:48:42 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+  public class Prime{    
+  public static void main(String args[]){    
+  int i,m=0,flag=0;      
+  int n=3;
+  m=n/2;      
+  if(n==0||n==1){  
+   System.out.println(n+" NO!");      
+  }else{  
+   for(i=2;i<=m;i++){      
+    if(n%i==0){      
+     System.out.println(n+" NO!");      
+     flag=1;      
+     break;      
+    }      
+   }      
+   if(flag==0)  { System.out.println(n+" YES"); }  
+  }//end of else  
+}    
+}
