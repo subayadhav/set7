3708320cff108a46da82d59a7fbeb2a23e068937
subayadhav <suba10499@gmail.com>
2019-06-03 20:42:44 +0530

    Update OddOrEven.java

--- OddOrEven.java
+++ OddOrEven.java
@@ -1,16 +1,18 @@
 import java.io.*;
 import java.util.*;
   public class OddOrEven{
-    public static void main(String args[]){
+    public static void main(String args[])
+    {
       Scanner sc1=new Scanner(System.in);
       System.out.println("Enter Two Numbers");
       int a=sc1.nextInt();
       int b=sc1.nextInt();
-      if((a+b)%2==00
+      if((a+b)%2==00)
       {
       System.out.println("Even num");
       }
-      else{
+      else
+      {
       System.out.println("Odd num");
       }
     }
